60c96b2a6517997a9ee55d659aa229f1ee0c6867
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 12:37:58 +0000

    exercise 16

--- test/exercise15.test.js
+++ test/exercise15.test.js
@@ -1,19 +1,19 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-var formatPeople = function(name, age) {
-  return '# name: ' + name + ', age: ' + age + '\n';
-};  
+// var formatPeople = function(name, age) {
+//   return '# name: ' + name + ', age: ' + age + '\n';
+// };  
 
-    suite('Unit Tests Exercise 15 - Approximation',function(){
-      // 15 - #match Asserts that the actual value 
-      // matches the second argument regular expression.
-      test('#match, #notMatch', function() {
-        var regex =  /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-        assert.match(formatPeople('John Doe', 35), regex);
-      });
-      test('#match, #notMatch', function() {
-        var regex =  /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-        assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
-      });
-    })
+//     suite('Unit Tests Exercise 15 - Approximation',function(){
+//       // 15 - #match Asserts that the actual value 
+//       // matches the second argument regular expression.
+//       test('#match, #notMatch', function() {
+//         var regex =  /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
+//         assert.match(formatPeople('John Doe', 35), regex);
+//       });
+//       test('#match, #notMatch', function() {
+//         var regex =  /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
+//         assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
+//       });
+//     })
