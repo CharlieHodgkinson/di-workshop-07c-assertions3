907525c0b887c6ac83e09c38ae61423bf1278766
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 12:33:10 +0000

    exercise 15

--- test/exercise14.test.js
+++ test/exercise14.test.js
@@ -1,13 +1,13 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-  suite('Unit Tests Exercise 14 - String include',function(){
-      // 14 - #include (on #notInclude ) works for strings too !! 
-      // It asserts that the actual string contains the expected substring
-      test('Checking String Inclusion 1', function(){
-        assert.include('Arrow', 'row', "Arrow contains row...");
-      });
-      test('Checking String Inclusion 2', function(){
-        assert.notInclude('dart', 'queue', "But a dart doesn't contain a queue");
-      });
-    });
+//   suite('Unit Tests Exercise 14 - String include',function(){
+//       // 14 - #include (on #notInclude ) works for strings too !! 
+//       // It asserts that the actual string contains the expected substring
+//       test('Checking String Inclusion 1', function(){
+//         assert.include('Arrow', 'row', "Arrow contains row...");
+//       });
+//       test('Checking String Inclusion 2', function(){
+//         assert.notInclude('dart', 'queue', "But a dart doesn't contain a queue");
+//       });
+//     });
