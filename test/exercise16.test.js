6cc87b5d7bee7bdb25ba6493117eac5f6cf9c491
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 12:43:32 +0000

    exercise 17

--- test/exercise16.test.js
+++ test/exercise16.test.js
@@ -1,32 +1,32 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
- // These variables are used in the tests. Don't Edit them.
- var Car = function() {
-  this.model = 'cedan';
-  this.engines = 1;
-  this.wheels = 4;
-};
+//  // These variables are used in the tests. Don't Edit them.
+//  var Car = function() {
+//   this.model = 'cedan';
+//   this.engines = 1;
+//   this.wheels = 4;
+// };
 
-var Plane = function() {
-  this.model = '737';
-  this.engines = ['left', 'right'];
-  this.wheels = 6;
-  this.wings = 2;
-};
+// var Plane = function() {
+//   this.model = '737';
+//   this.engines = ['left', 'right'];
+//   this.wheels = 6;
+//   this.wings = 2;
+// };
 
-var myCar = new Car();
-var airlinePlane = new Plane();
+// var myCar = new Car();
+// var airlinePlane = new Plane();
  
-    suite('Unit Tests Exercise 16 - Objects',function(){
-      // Use #property or #notProperty where appropriate
-      test('Object has property 1', function() {
-        assert.notProperty(myCar, 'wings', 'A car has not wings');
-      });
-      test('Object has property 2', function() {
-         assert.property(airlinePlane, 'engines', 'planes have engines');
-      });
-      test('Object has property 3', function() {
-        assert.property(myCar, 'wheels', 'Cars have wheels');
-      });
-    });
+//     suite('Unit Tests Exercise 16 - Objects',function(){
+//       // Use #property or #notProperty where appropriate
+//       test('Object has property 1', function() {
+//         assert.notProperty(myCar, 'wings', 'A car has not wings');
+//       });
+//       test('Object has property 2', function() {
+//          assert.property(airlinePlane, 'engines', 'planes have engines');
+//       });
+//       test('Object has property 3', function() {
+//         assert.property(myCar, 'wheels', 'Cars have wheels');
+//       });
+//     });
